477a582e91ed17b7204e21b38069f7d78fc5b219
mrstephenneal <stephen.neal14@gmail.com>
2020-08-13 17:59:47 -0400

    CUT todo

--- src/AbstractModel.php
+++ src/AbstractModel.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-// TODO: create package LaravelModels
+
 abstract class AbstractModel extends Model
 {
     use SoftDeletes,

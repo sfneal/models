355b32940cf85bcd57188227c81f2e86ee20f13a
Stephen Neal <stephen.neal14@gmail.com>
2021-01-27 12:28:26 -0500

    ADD test methods to AbstractModelTest

--- tests/AbstractModelTest.php
+++ tests/AbstractModelTest.php
@@ -7,6 +7,19 @@ namespace Sfneal\Models\Tests;
 class AbstractModelTest extends TestCase
 {
     /** @test */
+    public function hasAttribute()
+    {
+        $this->assertTrue($this->model->hasAttribute('name_first', true));
+        $this->assertTrue($this->model->hasAttribute('address', true));
+        $this->assertTrue($this->model->hasAttribute('name_full', false));
+        $this->assertTrue($this->model->hasAttribute('address_full', false));
+        $this->assertFalse($this->model->hasAttribute('first_name', true));
+        $this->assertFalse($this->model->hasAttribute('address_latest', true));
+        $this->assertFalse($this->model->hasAttribute('name_full_with_suffix', false));
+        $this->assertFalse($this->model->hasAttribute('address_city', false));
+    }
+
+    /** @test */
     public function getLabel()
     {
         $label = $this->model->getLabel();
@@ -14,11 +27,24 @@ class AbstractModelTest extends TestCase
     }
 
     /** @test */
-    public function hasAttribute()
+    public function idHash()
     {
-        $this->assertTrue($this->model->hasAttribute('name_first', true));
-        $this->assertTrue($this->model->hasAttribute('address', true));
-        $this->assertTrue($this->model->hasAttribute('name_full', false));
-        $this->assertTrue($this->model->hasAttribute('address_full', false));
+        $this->assertIsInt($this->model->id_hash);
+    }
+
+    /** @test */
+    public function getTableName()
+    {
+        $tableName = $this->model->getTableName();
+        $this->assertIsString($tableName);
+        $this->assertTrue($tableName == 'people');
+    }
+
+    /** @test */
+    public function getPrimaryKeyName()
+    {
+        $primaryKey = $this->model->getPrimaryKeyName();
+        $this->assertIsString($primaryKey);
+        $this->assertTrue($primaryKey == 'person_id');
     }
 }

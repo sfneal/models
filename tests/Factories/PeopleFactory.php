57230240e96f1438525cfc5f3ac4daf2ab149612
Stephen Neal <stephen.neal14@gmail.com>
2021-01-27 11:41:22 -0500

    MAKE People model & PeopleFactory for testing AbstractModel functionality
    ADD laravel/legacy-factories to composer dev requirements.

---
+++ tests/Factories/PeopleFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Sfneal\Models\Tests\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Sfneal\Models\Tests\Models\People;
+
+class PeopleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = People::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'name_first' => $this->faker->firstName,
+            'name_last' => $this->faker->lastName,
+            'email' => $this->faker->safeEmail,
+            'age' => $this->faker->numberBetween(21, 70),
+            'address' => $this->faker->streetAddress,
+            'city' => $this->faker->city,
+            'state' => $this->faker->state,
+            'zip' => $this->faker->postcode,
+        ];
+    }
+}
---
+++ tests/Models/People.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace Sfneal\Models\Tests\Models;
+
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Sfneal\Models\AbstractModel;
+use Sfneal\Models\Tests\Factories\PeopleFactory;
+
+class People extends AbstractModel
+{
+    use HasFactory;
+
+    protected $table = 'people';
+    protected $primaryKey = 'person_id';
+
+    protected $fillable = [
+        'person_id',
+        'name_first',
+        'name_last',
+        'email',
+        'age',
+        'address',
+        'city',
+        'state',
+        'zip',
+    ];
+
+    protected static function newFactory(): PeopleFactory
+    {
+        return new PeopleFactory();
+    }
+
+    public function getNameFullAttribute(): string
+    {
+        return "{$this->name_first} {$this->name_last}";
+    }
+
+    public function getNameLastFirstAttribute(): string
+    {
+        return "{$this->name_last}, {$this->name_first}";
+    }
+
+    public function getAddressFullAttribute(): string
+    {
+        return "{$this->address}, {$this->city}, {$this->state} {$this->zip}";
+    }
+}

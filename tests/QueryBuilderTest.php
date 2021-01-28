10fdd459c8b32222ca39069b7d348952ce1230b5
Stephen Neal <stephen.neal14@gmail.com>
2021-01-28 11:41:28 -0500

    ADD private whereLikeAssertions method for running repeated assertions

--- tests/QueryBuilderTest.php
+++ tests/QueryBuilderTest.php
@@ -2,17 +2,34 @@
 
 namespace Sfneal\Builders\Tests;
 
+use Sfneal\Builders\QueryBuilder;
 use Sfneal\Builders\Tests\Models\People;
 
 class QueryBuilderTest extends TestCase
 {
+    private function whereLikeAssertions (QueryBuilder $query)
+    {
+        $this->assertTrue($query instanceof QueryBuilder);
+        $this->assertSame($query->count(), 2);
+        $this->assertTrue(in_array('Stephen', $query->getFlatArray('name_first')));
+        $this->assertTrue(in_array('Richard', $query->getFlatArray('name_first')));
+    }
+
     /** @test */
     public function whereLike()
     {
         $query = People::query()->whereLike('name_last', 'Neal');
 
-        $this->assertSame($query->count(), 2);
-        $this->assertTrue(in_array('Stephen', $query->getFlatArray('name_first')));
-        $this->assertTrue(in_array('Richard', $query->getFlatArray('name_first')));
+        $this->whereLikeAssertions($query);
+    }
+
+    /** @test */
+    public function orWhereLike()
+    {
+        $query = People::query()
+            ->orWhereLike('name_first', 'Stephen')
+            ->orWhereLike('name_first', 'Richard');
+
+        $this->whereLikeAssertions($query);
     }
 }

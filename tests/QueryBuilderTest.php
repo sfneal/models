ba86efe4cb9bb366f7643b5b1c03b5031a0b8fd3
Stephen Neal <stephen.neal14@gmail.com>
2021-01-28 11:37:23 -0500

    ADD whereLike test to QueryBuilderTest

--- tests/QueryBuilderTest.php
+++ tests/QueryBuilderTest.php
@@ -2,14 +2,17 @@
 
 namespace Sfneal\Builders\Tests;
 
+use Sfneal\Builders\Tests\Models\People;
+
 class QueryBuilderTest extends TestCase
 {
     /** @test */
     public function whereLike()
     {
-//        $query = $this->models->query()->whereLike('name_last', 'Neal');
-//
-//        $this->assertTrue($query->count() == 2);
-        $this->assertTrue(true);
+        $query = People::query()->whereLike('name_last', 'Neal');
+
+        $this->assertSame($query->count(), 2);
+        $this->assertTrue(in_array('Stephen', $query->getFlatArray('name_first')));
+        $this->assertTrue(in_array('Richard', $query->getFlatArray('name_first')));
     }
 }

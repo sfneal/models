1208f38b34fe22bc9525f9b06eb6f6bead1d36a0
Stephen Neal <stephen.neal14@gmail.com>
2021-01-28 11:27:05 -0500

    FIX migration path

--- tests/Providers/BuildersTestingServiceProvider.php
+++ tests/Providers/BuildersTestingServiceProvider.php
@@ -14,7 +14,7 @@ class BuildersTestingServiceProvider extends ServiceProvider
         if (! class_exists('CreatePeopleTable')) {
             $this->publishes([
                 __DIR__.'/../migrations/create_people_table.php.stub' => database_path(
-                    'migrations/'.date('Y_m_d_His', time()).'_create_address_table.php'
+                    'migrations/'.date('Y_m_d_His', time()).'_create_people_table.php'
                 ),
             ], 'migration');
         }

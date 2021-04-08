f476cfebc60865b91986377d5631f235234e06a9
Stephen Neal <stephen.neal14@gmail.com>
2021-04-08 12:42:59 -0400

    REFACTOR use of `Illuminate\Database\Eloquent\Model` to `Sfneal\Models\Model`

--- src/Builders/QueryBuilder.php
+++ src/Builders/QueryBuilder.php
@@ -4,9 +4,9 @@ namespace Sfneal\Builders;
 
 use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder;
 use Sfneal\Builders\Traits\CountAndPaginate;
+use Sfneal\Models\Model;
 
 class QueryBuilder extends EloquentBuilder
 {

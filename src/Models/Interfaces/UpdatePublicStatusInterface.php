c1caac4e1771cd8dcbb1e1dd571ff1b9004bd212
Stephen Neal <stephen.neal14@gmail.com>
2021-07-15 14:26:28 -0400

    Revert "CUT `UpdatePublicStatusInterface`"
    This reverts commit 865433d5

---
+++ src/Models/Interfaces/UpdatePublicStatusInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sfneal\Models\Interfaces;
+
+interface UpdatePublicStatusInterface
+{
+    /**
+     * Determine if a Model is identified as 'public'.
+     *
+     * @return bool
+     */
+    public function isPublic(): bool;
+}
--- src/Models/ModelWithPublicStatus.php
+++ src/Models/ModelWithPublicStatus.php
@@ -3,9 +3,10 @@
 namespace Sfneal\Models;
 
 use Sfneal\Models\Interfaces\IsPublicInterface;
+use Sfneal\Models\Interfaces\UpdatePublicStatusInterface;
 use Sfneal\Models\Traits\PublicStatusTrait;
 
-abstract class ModelWithPublicStatus extends Model implements IsPublicInterface
+abstract class ModelWithPublicStatus extends Model implements IsPublicInterface, UpdatePublicStatusInterface
 {
     use PublicStatusTrait;
 }

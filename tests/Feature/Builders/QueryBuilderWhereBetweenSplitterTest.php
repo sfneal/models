2a10eeda937fb3341ef7feb622a990a025aca77e
Stephen Neal <stephen.neal14@gmail.com>
2021-07-15 13:59:49 -0400

    MAKE `QueryBuilderWhereBetweenSplitterTest` for testing `WhereBetweenSplitter` builder trait

---
+++ tests/Feature/Builders/QueryBuilderWhereBetweenSplitterTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Sfneal\Models\Tests\Feature\Builders;
+
+use Sfneal\Builders\QueryBuilder;
+use Sfneal\Models\Tests\Assets\Builders\PeopleBuilder;
+use Sfneal\Models\Tests\Assets\Models\People;
+use Sfneal\Models\Tests\BuilderTestCase;
+
+class QueryBuilderWhereBetweenSplitterTest extends BuilderTestCase
+{
+    public function ageRangeProvider(): array
+    {
+        $ranges = [
+            [rand(21, 70), rand(21, 70)],
+            [rand(21, 70), rand(21, 70)],
+            [rand(21, 70), rand(21, 70)],
+            [rand(21, 70), rand(21, 70)],
+            [rand(21, 70), rand(21, 70)],
+        ];
+
+        $data = [];
+        foreach ($ranges as $range) {
+            $min = min($range);
+            $max = max($range);
+            $data[] = [$min, $max, [$min, $max]];
+            $data[] = [$min, $max, "{$min}-{$max}"];
+        }
+        return $data;
+    }
+
+    /**
+     * @test
+     * @dataProvider ageRangeProvider
+     */
+    public function whereBetweenSplitter(int $min, int $max, $range)
+    {
+        $query = People::query()->whereBetweenSplitter('age', $range);
+
+        $this->assertTrue($query instanceof QueryBuilder);
+        $this->assertTrue($query instanceof PeopleBuilder);
+        $query->pluck('age')->each(function (int $age) use ($min, $max) {
+            $this->assertGreaterThanOrEqual($min, $age);
+            $this->assertLessThanOrEqual($max, $age);
+        });
+    }
+}

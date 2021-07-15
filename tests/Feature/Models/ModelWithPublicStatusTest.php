97fd52d30b1fe9986d8f09cc7c1ee987402bc85e
Stephen Neal <stephen.neal14@gmail.com>
2021-07-15 14:35:11 -0400

    MAKE `ModelWithPublicStatusTest` for testing public status traits & interfaces

---
+++ tests/Feature/Models/ModelWithPublicStatusTest.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace Sfneal\Models\Tests\Feature\Models;
+
+
+use Sfneal\Models\Tests\Assets\Models\People;
+use Sfneal\Models\Tests\BuilderTestCase;
+
+class ModelWithPublicStatusTest extends BuilderTestCase
+{
+    /** @test */
+    public function isPublic()
+    {
+        People::query()
+            ->where('public_status', '=', 1)
+            ->get()
+            ->each(function (People $people) {
+                $this->assertTrue($people->isPublic());
+            });
+    }
+
+    /** @test */
+    public function isPrivate()
+    {
+        People::query()
+            ->where('public_status', '=', 0)
+            ->get()
+            ->each(function (People $people) {
+                $this->assertTrue($people->isPrivate());
+            });
+    }
+
+    /** @test */
+    public function updatePublicStatus()
+    {
+        $model = People::factory()->create(['public_status' => null]);
+        $this->assertNull($model->public_status);
+
+        $model->updatePublicStatus(1);
+        $this->assertTrue($model->isPublic());
+
+        $model->updatePublicStatus(10);
+        $this->assertTrue($model->isPrivate());
+    }
+}

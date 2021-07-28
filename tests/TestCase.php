742c6a695dc61996f28d5c88732527256d55b106
Stephen Neal <stephen.neal14@gmail.com>
2021-07-28 14:59:54 -0400

    CUT 'create_people_hard_table.php.stub' by adding constructor with params to parent migration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -34,8 +34,6 @@ class TestCase extends OrchestraTestCase
     {
         include_once __DIR__ . '/database/migrations/create_people_table.php.stub';
         (new \CreatePeopleTable())->up();
-
-        include_once __DIR__ . '/database/migrations/create_people_hard_table.php.stub';
-        (new \CreatePeopleHardTable())->up();
+        (new \CreatePeopleTable('people_hard', false))->up();
     }
 }

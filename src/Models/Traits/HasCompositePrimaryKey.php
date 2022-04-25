279926be5b3262c629783ebfe73544652f7291ac
Stephen Neal <stephen.neal14@gmail.com>
2022-04-25 13:41:28 -0400

    MAKE `HasCompositePrimaryKey` trait for pivot models with composite primary keys

---
+++ src/Models/Traits/HasCompositePrimaryKey.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Sfneal\Models\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+
+trait HasCompositePrimaryKey
+{
+    /**
+     * Get the value indicating whether the IDs are incrementing.
+     *
+     * @return bool
+     */
+    public function getIncrementing(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Set the keys for a save update query.
+     *
+     * @param  Builder  $query
+     * @return Builder
+     */
+    protected function setKeysForSaveQuery($query): Builder
+    {
+        $keys = $this->getKeyName();
+        if(!is_array($keys)){
+            return parent::setKeysForSaveQuery($query);
+        }
+
+        foreach($keys as $keyName){
+            $query->where($keyName, '=', $this->getKeyForSaveQuery($keyName));
+        }
+
+        return $query;
+    }
+
+    /**
+     * Get the primary key value for a save query.
+     *
+     * @param string|null $keyName
+     * @return mixed
+     */
+    protected function getKeyForSaveQuery(string $keyName = null): mixed
+    {
+        if(is_null($keyName)){
+            $keyName = $this->getKeyName();
+        }
+
+        if (isset($this->original[$keyName])) {
+            return $this->original[$keyName];
+        }
+
+        return $this->getAttribute($keyName);
+    }
+}
